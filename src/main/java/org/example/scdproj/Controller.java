c79f9ddf1dc4c51f3f76e3b029295fd9014fe841
Usman Ali <l227984@gmail.com>
2024-11-25 17:21:52 +0500

    Removed old practice files

---
+++ src/main/java/org/example/scdproj/Controller.java
@@ -0,0 +1,10 @@
+package org.example.scdproj;
+
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    private void handleButtonClick() {
+        System.out.println("FXML Button clicked!");
+    }
+}
---
+++ src/main/java/org/example/scdproj/MainApp.java
@@ -0,0 +1,21 @@
+package org.example.scdproj;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("mainFxml.fxml"));
+        primaryStage.setTitle("FXML Example");
+        primaryStage.setScene(new Scene(root, 300, 200));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
