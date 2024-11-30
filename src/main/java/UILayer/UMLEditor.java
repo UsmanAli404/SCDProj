76940f9d6df3bbba892c840280466bf4365a6c5e
Usman Ali <l227984@gmail.com>
2024-11-30 19:24:25 +0500

    landingPage ui

---
+++ src/main/java/UILayer/UMLEditor.java
@@ -0,0 +1,16 @@
+package UILayer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UMLEditor extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,4 @@
 module org.example.scdproj {
     requires javafx.controls;
     requires javafx.fxml;
-
-    exports UILayer to javafx.graphics;
 }
