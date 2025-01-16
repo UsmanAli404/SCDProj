5ab2e2cbed0359312cc9b6245c33d15048179e68
Usman Ali <l227984@gmail.com>
2025-01-16 15:22:03 +0500

    - removed useless line files (aggregation, composition, inheritance) - modified line display between connected classes - did some refactoring of the code in ClassDiagramController.java

---
+++ src/main/java/BusinessLayer/Models/Components/ClassDiagramComponents/Classes/ClassObj.java
@@ -0,0 +1,11 @@
+package BusinessLayer.Models.Components.ClassDiagramComponents.Classes;
+
+import java.util.ArrayList;
+
+public abstract class ClassObj{
+    private ArrayList<Function> functions;
+
+    public ClassObj(){
+
+    }
+}
